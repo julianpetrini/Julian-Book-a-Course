221ecdac1621d85ebb958b758e3a18290b50d1ca
julianpetrini <bwmisojos@gmail.com>
2022-02-24 16:32:54 +0100

    nav_bar_and_basics_

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 
-document.getElementById("test").innerHTML="hello world"
+//document.getElementById("test").innerHTML="hello world"
+
+// nav toggle - select button and links
+const navToggle=document.querySelector("#navToggle");
+const nav =document.querySelector("#nav-links");
+
+//add event listener. Connect css 
+navToggle.addEventListener("click",() =>{
+    nav.classList.toggle("nav-open")
+})
