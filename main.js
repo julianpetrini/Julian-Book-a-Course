d04681223b697ab17c235d4bead51e3900817918
julianpetrini <bwmisojos@gmail.com>
2022-03-01 14:25:13 +0100

    ALMOST DONE_

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-
-//document.getElementById("test").innerHTML="hello world"
-
-// nav toggle - select button and links
-const navToggle=document.querySelector("#navToggle");
-const nav =document.querySelector("#nav-links");
-
-//add event listener. Connect css 
-navToggle.addEventListener("click",() =>{
-    nav.classList.toggle("nav-open")
-})
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
